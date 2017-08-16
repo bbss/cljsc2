cb2117f37a44bdb4e051b56d5d084b958a8507d5
bbss <baruchberger@gmail.com>
2017-08-17 00:18:53 +0900

    wip, joins game

---
+++ src/cljsc2/clj/core.clj
@@ -0,0 +1,96 @@
+(ns cljsc2.clj.core
+  (:use flatland.protobuf.core
+        [clojure.java.shell :only [sh]]
+        lucid.mind)
+  (:require
+   [flatland.protobuf.schema :only [field-schema]]
+   [flatland.useful.fn :only [fix]]
+   [clojure.java.io :only [input-stream output-stream]]
+   [aleph.http :as http]
+   [manifold.stream :as s]
+   flatland.useful.utils)
+  (:import
+   (flatland.protobuf PersistentProtocolBufferMap PersistentProtocolBufferMap$Def PersistentProtocolBufferMap$Def$NamingStrategy Extensions)
+   (com.google.protobuf GeneratedMessage CodedInputStream Descriptors$Descriptor Descriptors$EnumDescriptor)
+   (java.io InputStream OutputStream)
+   (clojure.lang Reflector)
+   SC2APIProtocol.Raw
+   SC2APIProtocol.Ui
+   SC2APIProtocol.Data
+   SC2APIProtocol.Debug
+   SC2APIProtocol.Query
+   SC2APIProtocol.Common
+   SC2APIProtocol.Sc2Api
+   SC2APIProtocol.Spatial
+   SC2APIProtocol.ScoreOuterClass))
+
+
+(future
+  (sh "/Applications/StarCraft II/Versions/Base55958/SC2.app/Contents/MacOS/SC2"
+      "-listen" "127.0.0.1" "-port" "5000" "-displayMode" "0"))
+
+(def Request
+  (protodef SC2APIProtocol.Sc2Api$Request))
+
+(def RequestCreateGame
+  (protodef SC2APIProtocol.Sc2Api$RequestCreateGame))
+
+(def LocalMap
+  (protodef SC2APIProtocol.Sc2Api$LocalMap))
+
+(def connection (http/websocket-client "ws://127.0.0.1:5000/sc2api"))
+
+(def RequestPing (protodef SC2APIProtocol.Sc2Api$RequestPing))
+
+(def PlayerSetup (protodef SC2APIProtocol.Sc2Api$PlayerSetup))
+
+(def player-builder
+  (doto (SC2APIProtocol.Sc2Api$PlayerSetup/newBuilder)
+    (.setType  (SC2APIProtocol.Sc2Api$PlayerType/forNumber 1))
+    (.setRace  (SC2APIProtocol.Sc2Api$Race/forNumber 1))))
+
+(s/put! @connection
+        (protobuf-dump
+         (protobuf Request :create-game
+                   (protobuf RequestCreateGame
+                             :local-map
+                             {:map-path "/Applications/StarCraft II/Maps/Melee/Simple64.SC2Map"}
+                             :player-setup [(protobuf-load PlayerSetup (.toByteArray (.build player-builder)))]))))
+
+(s/put! @connection
+        (protobuf-dump
+         (protobuf Request :ping (protobuf (protodef SC2APIProtocol.Sc2Api$RequestPing)))))
+
+(def took (s/take! @connection))
+
+(def Response
+  (protodef SC2APIProtocol.Sc2Api$Response))
+
+(protobuf-load Response @took)
+
+(protobuf-load (protodef SC2APIProtocol.Sc2Api$ResponseJoinGame) @took)
+
+(protobuf (protodef SC2APIProtocol.Sc2Api$PlayerSetup))
+
+(protobuf-schema SC2APIProtocol.Sc2Api$PlayerSetup)
+
+(protobuf-schema Request)
+
+(def RequestJoinGame (protodef SC2APIProtocol.Sc2Api$RequestJoinGame))
+
+(defn send-request [connection request]
+  (s/put! @connection
+          (protobuf-dump request)))
+
+(def join-game-request
+  (protobuf Request :join-game
+            (protobuf-load RequestJoinGame
+                           (-> (doto (SC2APIProtocol.Sc2Api$RequestJoinGame/newBuilder)
+                                 (.setRace (SC2APIProtocol.Sc2Api$Race/forNumber 1))
+                                 (.setOptions (-> (doto (SC2APIProtocol.Sc2Api$InterfaceOptions/newBuilder))
+                                                  .build))
+                                 )
+                               .build
+                               .toByteArray))))
+
+(send-request connection join-game-request)
